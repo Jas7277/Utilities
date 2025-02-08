017b374185a7384223e34b4586fce4e1e7ed0d15
Jason <jasonsworld21@yahoo.com>
2025-02-08 16:02:49 -0500

    Adds Coord struct for Unity
    This is a custom struct I use regularly with Unity Engine.

---
+++ coord.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public struct Coord
+{
+    public int x;
+    public int y;
+
+    public Coord(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Coord(Vector2 v)
+    {
+        this.x = (int)v.x;
+        this.y = (int)v.y;
+    }
+
+    public Coord(Vector3 v)
+    {
+        this.x = (int)v.x;
+        this.y = (int)v.z;
+    }
+
+    public Coord(Vector2Int v)
+    {
+        this.x = v.x;
+        this.y = v.y;
+    }
+
+    public Coord(Vector3Int v)
+    {
+        this.x = v.x;
+        this.y = v.z;
+    }
+
+    public static int SqrDistance (Coord a, Coord b)
+    {
+        return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+    }
+
+    public static float Distance (Coord a, Coord b)
+    {
+        return (float) System.Math.Sqrt(SqrDistance(a, b));
+    }
+
+    public static bool AreNeighbours (Coord a, Coord b)
+    {
+        return System.Math.Abs(a.x - b.x) <= 1 && System.Math.Abs(a.y - b.y) <= 1;
+    }
+
+    public static bool operator == (Coord a, Coord b)
+    {
+        return a.x == b.x && a.y == b.y;
+    }
+
+    public static bool operator != (Coord a, Coord b)
+    {
+        return !(a == b);
+    }
+
+    public static implicit operator Vector2(Coord a)
+    {
+        return new Vector2 (a.x, a.y);
+    }
+
+    public static implicit operator Vector3(Coord a)
+    {
+        return new Vector3(a.x, 0, a.y);
+    }
+
+    public override string ToString()
+    {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public override int GetHashCode()
+    {
+        return 0;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return (Coord)obj == this;
+    }
+}
